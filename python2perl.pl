24cb2708dbf3786219da4b30a2f0898e3d103dfd
Rachael Zhou <rachaelzhou95@gmail.com>
2014-10-06 16:40:29 +1100

    proper one

--- python2perl.pl
+++ python2perl.pl
@@ -4,22 +4,42 @@
 # written by andrewt@cse.unsw.edu.au September 2014
 
 while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
-    
-        # translate #! line 
-        
+    if ($line =~ /^#!/ && $. == 1) { # translate #! line 
         print "#!/usr/bin/perl -w\n";
-    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-    
-        # Blank & comment lines can be passed unchanged
-        
+
+    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) { # Blank & comment lines can be passed unchanged
         print $line;
-    } elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
-    
+
+    } 
+    if ($line =~ /=/) {
+        @arrayLine = split (" ", $line); # puts each thing in an array
+        $i = 0;
+        $varName  = $arrayLine[$i - 1];
+        $VarValue = $arrayLine[$i + 2];
+        
+    }
+
+    elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+        @arrayLine = split (" ", $line); # puts each thing in an array
+        $i = 0;
+        while ($i <= $#arrayLine) {
+            $temp = $arrayLine[$i];      # goes through the array and sets temp to each elt in the array
+            
+            if ($temp =~ /print/) {      # if the elt is a print line then..
+                print "print";
+            }
+
+            else { #print what ever is in the double quotes 
+                print " $temp";
+            }
+            $i++;
+        }
+        print "; print \"\\n\";\n"; #force it to print/output the new line
+
+
         # Python's print print a new-line character by default
         # so we need to add it explicitly to the Perl print statement
-        
-        print "print \"$1\\n\";\n";
+    
     } else {
     
         # Lines we can't translate are turned into comments
