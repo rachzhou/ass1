f540ad14be152fac27943c51c4637d97c8f8e304
Rachael Zhou <rachaelzhou95@gmail.com>
2014-10-06 17:53:41 +1100

    variables

--- python2perl.pl
+++ python2perl.pl
@@ -3,6 +3,9 @@
 # http://www.cse.unsw.edu.au/~cs2041/assignments/python2perl
 # written by andrewt@cse.unsw.edu.au September 2014
 
+use Scalar::Util qw(looks_like_number);
+my $math = "[\+\-\/\=\*]";
+
 while ($line = <>) {
     if ($line =~ /^#!/ && $. == 1) { # translate #! line 
         print "#!/usr/bin/perl -w\n";
@@ -11,30 +14,44 @@ while ($line = <>) {
         print $line;
 
     } 
-    if ($line =~ /=/) {
+
+    elsif ($line =~ /^\s*\w+\s*\=\s*\w*$/) { # eg var = 43;
+        
         @arrayLine = split (" ", $line); # puts each thing in an array
         $i = 0;
-        $varName  = $arrayLine[$i - 1];
-        $VarValue = $arrayLine[$i + 2];
-        
+        while ($i <= $#arrayLine) {
+            $temp = $arrayLine[$i];
+            if ($temp =~ /$math/){
+                print " $temp ";
+            }
+            else {
+                print looks_like_number($temp)? '' : '$' , "$temp"; #if the elt is a string print a $ infront of it coz its a variable
+            }
+
+            #$varName  = $arrayLine[$i - 1];
+            #$VarValue = $arrayLine[$i + 2];
+            #print "\$$varName = $VarValue;\n";
+            $i++;
+        }
+        print "\;\n";
     }
 
-    elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+    elsif ($line =~ /^\s*print/) {
         @arrayLine = split (" ", $line); # puts each thing in an array
         $i = 0;
         while ($i <= $#arrayLine) {
             $temp = $arrayLine[$i];      # goes through the array and sets temp to each elt in the array
             
             if ($temp =~ /print/) {      # if the elt is a print line then..
-                print "print";
+                print "print ";
             }
 
-            else { #print what ever is in the double quotes 
-                print " $temp";
+            else {
+                print looks_like_number($temp)? '' : '$' , "$temp"; #if the elt is a string print a $ infront of it coz its a variable
             }
             $i++;
         }
-        print "; print \"\\n\";\n"; #force it to print/output the new line
+        print ";\nprint \"\\n\";\n"; #force it to print/output the new line
 
 
         # Python's print print a new-line character by default
